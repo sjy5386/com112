07255cd623486b6b405c69155d959166325c45ef
Seokjin Yoon <sjy5386@gmail.com>
2020-01-14 21:30:35 +0900

    Add Processor

---
+++ com112/Processor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace com112
+{
+    class Processor
+    {
+        private string name;
+        private double speed;
+
+        public Processor()
+        {
+            ManagementClass managementClass = new ManagementClass("Win32_Processor");
+            ManagementObjectCollection managementObjectCollection = managementClass.GetInstances();
+            foreach (ManagementObject managementObject in managementObjectCollection)
+            {
+                name = (string)managementObject["name"];
+                speed = (UInt32)managementObject.Properties["CurrentClockSpeed"].Value / 1000;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public double Speed
+        {
+            get
+            {
+                return speed;
+            }
+        }
+    }
+}
