b568b8483350aef2806fc7bf755fcb709d990eaf
Seokjin Yoon <sjy5386@gmail.com>
2020-01-14 22:48:23 +0900

    Add Storage

---
+++ com112/Drive.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com112
+{
+    class Drive
+    {
+        private string name;
+        private UInt64 size;
+
+        public Drive(string name, UInt64 size)
+        {
+            this.name = name;
+            this.size = size;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        public UInt64 Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+    }
+}
---
+++ com112/Storage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace com112
+{
+    class Storage
+    {
+        private List<Drive> drives = new List<Drive>(4);
+
+        public Storage()
+        {
+            ManagementClass managementClass = new ManagementClass("Win32_DiskDrive");
+            ManagementObjectCollection managementObjectCollection = managementClass.GetInstances();
+            foreach (ManagementObject managementObject in managementObjectCollection)
+            {
+                string name = (string)managementObject["Model"];
+                UInt64 size = (UInt64)managementObject["Size"];
+            }
+        }
+
+        public Drive GetDrive(int index)
+        {
+            return drives[index];
+        }
+
+        public int GetSize()
+        {
+            return drives.Count;
+        }
+    }
+}
