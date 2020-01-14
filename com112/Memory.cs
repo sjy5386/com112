560ec12267468e7bc841ef2b82b5c3fd89436ca4
Seokjin Yoon <sjy5386@gmail.com>
2020-01-14 22:12:51 +0900

    Add Memory

---
+++ com112/Memory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace com112
+{
+    class Memory
+    {
+        private List<MemoryCard> memoryCards = new List<MemoryCard>(4);
+
+        public Memory()
+        {
+            ManagementClass managementClass = new ManagementClass("Win32_PhysicalMemory");
+            ManagementObjectCollection managementObjectCollection = managementClass.GetInstances();
+            foreach (ManagementObject managementObject in managementObjectCollection)
+            {
+                string manufacturer = (string)managementObject["Manufacturer"];
+                UInt64 size = (UInt64)managementObject["Capacity"];
+                MemoryCard memoryCard = new MemoryCard(manufacturer, size);
+                memoryCards.Add(memoryCard);
+            }
+        }
+
+        public UInt64 GetTotalSize()
+        {
+            UInt64 totalSize = 0;
+            foreach (MemoryCard memoryCard in memoryCards)
+            {
+                totalSize += memoryCard.Size;
+            }
+            return totalSize;
+        }
+    }
+}
---
+++ com112/MemoryCard.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com112
+{
+    class MemoryCard
+    {
+        private string manufacturer;
+        private UInt64 size;
+
+        public MemoryCard(string manufacturer, UInt64 size)
+        {
+            this.manufacturer = manufacturer;
+            this.size = size;
+        }
+
+        public string Manufacturer
+        {
+            get
+            {
+                return manufacturer;
+            }
+        }
+
+        public UInt64 Size
+        {
+            get
+            {
+                return size;
+            }
+        }
+    }
+}
